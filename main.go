f40c43be4f5a2f43754a365ecee42782cce84e49
siluk00 <sil-uk@hotmail.com>
2025-12-31 15:26:28 -0300

    testing worker

--- main.go
+++ main.go
@@ -78,6 +78,39 @@ func main() {
 	time.Sleep(time.Second * 5)
 	fmt.Printf("stopping container %s\n", createResult.ContainerId)
 	_ = stopContainer(dockerTask, createResult.ContainerId)
+
+	db := make(map[uuid.UUID]*task.Task)
+	w = worker.Worker{
+		Queue: *queue.New(),
+		Db:    db,
+	}
+
+	t = task.Task{
+		ID:    uuid.New(),
+		Name:  "test-container-1",
+		State: task.Scheduled,
+		Image: "strm/helloworld-http",
+	}
+
+	fmt.Println("starting task")
+	w.AddTask(t)
+	result := w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	t.ContainerID = result.ContainerId
+	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerID)
+	fmt.Println("Sleepy time")
+	time.Sleep(time.Second * 30)
+
+	fmt.Printf("stopping task %s\n", t.ID)
+	t.State = task.Completed
+	w.AddTask(t)
+	result = w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
 }
 
 func createContainer() (*task.Docker, *task.DockerResult) {
